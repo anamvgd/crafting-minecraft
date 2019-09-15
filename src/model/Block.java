28be05f9ed935860fff262c4cd90c889d33f9f09
Ana Muñoz <munoz.am.118@gmail.com>
2019-09-15 18:17:01 -0500

    added new class for the hash table

--- src/model/Block.java
+++ src/model/Block.java
@@ -2,44 +2,38 @@ package model;
 
 import java.util.LinkedList;
 
-public class Block<K, V> implements Box<K, V>{
+public class Block<K, V> {
 	
 	public static final int MAX_BLOCKS = 64;
-	private LinkedList<Block<K, V>>[] b;
+	private LinkedList<Cube<K, V>>[] b;
 	private K key;
 	private V value;
 	
-	public Block(K k, V v) {
+	
+	
+	public Block() {
 		super();
 		b = new LinkedList[MAX_BLOCKS];
 		for (int i = 0; i < MAX_BLOCKS; i++) {
 			b[i] = null;
 		}
 		
-		this.key = k;
-		this.value = v;
 	}
 
-	@Override
-	public K getKey() {
-		return key;
-	}
-	
-
-	public Block<K, V> getBlock(K key) {
+	public Cube<K, V> getCube(K key) {
 		if(key==null) {
 			return null;
 		}
 		
 		int index = key.hashCode() % MAX_BLOCKS;
-		LinkedList<Block<K, V>> items = b[index];
+		LinkedList<Cube<K, V>> items = b[index];
 		
 		if(items == null) {
 			return null;
 		}
 		
-		for (Block<K, V> item : items) {
-			if(item.key.equals(key)) {
+		for (Cube<K, V> item : items) {
+			if(item.getKey().equals(key)) {
 				return item;
 			}
 		}
@@ -49,33 +43,33 @@ public class Block<K, V> implements Box<K, V>{
 	
 	
 	public V getValue(K key) {
-		Block<K, V> item = getBlock(key);
+		Cube<K, V> item = getCube(key);
 		
 		if(item == null) {
 			return null;
 		} else {
-			return item.value;
+			return item.getValue();
 		}
 	}
 	
 	public void insert(K key, V value) {
 		int index = key.hashCode() % MAX_BLOCKS;
-		LinkedList<Block<K, V>> items = b[index];
+		LinkedList<Cube<K, V>> items = b[index];
 		
 		if(items == null) {
-			items = new LinkedList<Block<K, V>>();
+			items = new LinkedList<Cube<K, V>>();
 			
-			Block<K, V> item = new Block<K, V>(key, value);
+			Cube<K, V> item = new Cube<K, V>(key, value);
 			items.add(item);
 			b[index] = items;
 		} else { 
-			for (Block<K, V> item : items) {
+			for (Cube<K, V> item : items) {
 				if(item.getKey().equals(key)) {
 					item.setValue(value);
 				}
 			}
 			
-			Block<K, V> item = new Block<K, V>(key, value);
+			Cube<K, V> item = new Cube<K, V>(key, value);
 			item.setKey(key);
 			item.setValue(value);
 			
@@ -87,13 +81,13 @@ public class Block<K, V> implements Box<K, V>{
 	
 	public void delete(K key) {
 		int index = key.hashCode() % MAX_BLOCKS;
-		LinkedList<Block<K,V>> items = b[index];
+		LinkedList<Cube<K,V>> items = b[index];
 		
 		if(items == null) {
 			
 		}
 		
-		for (Block<K, V> block : items) {
+		for (Cube<K, V> block : items) {
 			if(block.getKey().equals(key)) {
 				items.remove(block);
 			}
@@ -102,13 +96,5 @@ public class Block<K, V> implements Box<K, V>{
 		
 	}
 	
-	public void setValue(V value) {
-		this.value = value;
-	}
-	
-	public void setKey(K key) {
-		this.key = key;
-	}
-
 	
 }
---
+++ src/model/Cube.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Cube<K, V> implements Box<K, V>{
+	
+	private K key;
+	private V value;
+	
+	public Cube(K k, V v) {
+		this.key = k;
+		this.value = v;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
